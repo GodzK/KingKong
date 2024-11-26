d23f6a166af1d922787545f982d58626bdf4814a
Phakaphol Dherachaisuphakij <64467457+GodzK@users.noreply.github.com>
2024-11-26 10:22:50 +0700

    add 2 code

--- another/bank.py
+++ another/bank.py
@@ -2,37 +2,62 @@ class bl:
     def __init__(self):
         self.userdata = {}
     def addnewuser(self, idregis, userregis):
-        self.userdata[idregis] = userregis
+        self.userdata[idregis] = {"name" : userregis , "money" : 0 }
         print(self.userdata)
     def checkuser(self, idlogin,userlogin):
         if idlogin in self.userdata:
             return True
         else:
             return False
-
+    def savemoney(self,idlogin,money):
+        self.userdata[idlogin]["money"] += money
+    def returnmoney(self,idlogin):
+        return self.userdata[idlogin]["money"]
+    def checkidregis(self,idregis):
+        if 0 < int(idregis) < 100:
+            return True
+        else:
+            return ValueError
+    def checkuserregis(self,userregis):
+        if userregis == str:
+            return True
+        else:
+            return ValueError
+        
+    
+        
 class ui:
     def __init__(self,bl_instance):
         self.bl = bl_instance
 
     def mainpage(self):
-        print("welcome to Bank")
-        print("Type [1]  to go register")
-        print("Type [2] to go to login")
-        userinp = input("Type Your Choice ! : ")
-        try:
-            match int(userinp):
-                case 1:
-                    self.register()
-                case 2:
-                    self.login()
-        except ValueError:
-            print("invalid input")
+        while True:
+            print("welcome to Bank")
+            print("Type [1]  to go register")
+            print("Type [2] to go to login")
+            userinp = input("Type Your Choice ! : ")
+            try:
+                match int(userinp):
+                    case 1:
+                        self.register()
+                    case 2:
+                        self.login()               
+            except ValueError:
+                print("invalid input")
 
     def register(self):
-        idregis = input("type your id : ")
-        userregis = input("type your name : ") 
-        self.bl.addnewuser(idregis , userregis)
-        self.mainpage()
+        while True:
+            try:
+                idregis = input("type your id : ")
+                self.bl.checkidregis(idregis)
+                userregis = input("type your name : ") 
+                self.bl.checkuserregis(userregis)
+                self.bl.addnewuser(idregis , userregis)
+                self.mainpage()
+            except TypeError:
+                raise("your data is wrong")
+                
+        
 
     def login(self):
         print("Login page")
@@ -40,35 +65,40 @@ class ui:
         userlogin = input("Type Your Name : ")
         try:
             if self.bl.checkuser(idlogin,userlogin):
-                self.menu()
+                self.menu(idlogin,userlogin)
             else:
                 print("You have not an account")
-                self.mainpage()
+                self.menu(idlogin,userlogin)
         except TypeError:
                 raise ("Your Type data is Error!!!")
         except ValueError:
                 raise ("You Pass The wrong Data!!!")
+                  
+
+    def menu(self,idlogin,userlogin):
+        while True: 
+            print(f"Welcome {userlogin}")
+            print("[1] to safe money")
+            print("[2] to see your money")
+            print("[3] to exit")
+            userchoice = input("Type Your Choice : ")
+            match int(userchoice):
+                case 1:
+                    try:
+                        money = int(input("Type Your money : "))
+                        self.bl.savemoney(idlogin,money)
+                        print("Save!!")
+                        self.menu(idlogin,userlogin)
+                    except TypeError:
+                        return ("Your type is Error")
+                case 2 :
+                    print(f"You Have {self.bl.returnmoney(idlogin)} Bath")
+                    self.menu(idlogin,userlogin)
+                case 3:
+                    print("See You later")
+                    break
                 
-            
-        
-            
-
-    def menu(self):
-        print("This is Menu Page")
-
-            
-
-
-            
-        
-        
-
-        
-            
-
-
-
-
+                
     
 def main():
     bl_instance = bl()
@@ -77,7 +107,3 @@ def main():
 
 
 main()
-
-
-        
-        
